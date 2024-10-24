1ae6f55a6aeb23aa72104217de38ef20fcc1c665
JaredBaileyDuke <jared.l.bailey@duke.edu>
2024-10-24 14:23:39 -0400

    initial code for checkers board and pieces

--- src/board.py
+++ src/board.py
@@ -2,12 +2,47 @@ class Board:
     def __init__(self):
         self.board = self.create_board()
 
+
     def create_board(self):
+        """
+        Create an 8x8 board
+        - '#' for empty squares
+        - ' ' for squares that cannot be moved to.
+        """
         board = []
+        square_notation = 1
+        row_counter = 0
         for row in range(8):
-            board.append([None] * 8)
+            # Create a new row
+            new_row = []
+            for col in range(8):
+                if (col + row) % 2 == 0:
+                    new_row.append(0)
+                else:
+                    new_row.append(square_notation)
+                    square_notation += 1
+            board.append(new_row)
+            row_counter += 1
+
         return board
 
-    def display_board(self):
-        for row in self.board:
-            print(" ".join([str(piece) if piece else '.' for piece in row]))
+    def display_blank_board(self):
+        print("Blank Board:")
+        print("   A B C D E F G H")
+        print(" +----------------+")
+        for i, row in enumerate(self.board):
+            print(f"{7 - i + 1}|", end="")
+            for square in row:
+                if square == 0:
+                    print("  ", end="")
+                elif len(str(square)) == 1:
+                    print(f" {square}", end="")
+                else:
+                    print(f"{square}", end="")
+            print("|")
+        
+        
+# Example usage
+if __name__ == "__main__":
+    game_board = Board()
+    game_board.display_blank_board()
