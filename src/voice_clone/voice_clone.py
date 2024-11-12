70db7f0ad8c5d32adec8cf98e3fbac412a97fb16
JaredBaileyDuke <jared.l.bailey@duke.edu>
2024-11-12 13:22:20 -0500

    Added working premade smacktalk audio file

--- src/voice_clone/voice_clone.py
+++ src/voice_clone/voice_clone.py
@@ -4,17 +4,25 @@ Premade audio files are available in the premade_audio folder
 On the fly audio can be generated through the API call
 """
 
-from playsound import playsound
+import pygame
+import random
 
-def play_audio(audio_file):
+def play_audio():
     """
     Choose a random smackTalk audio file from the premade_audio folder and play it
     """
     # Choose a random number between 1 and 50, inclusive
     random_number = random.randint(1, 50)
     # Play the audio file
-    audio_file = f"premade_audio/smackTalk_{random_number}.wav"
-    playsound(audio_file)
+    audio_file = f"premade_audio/smackTalk_{random_number}.mp3"
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():  # Wait for the music to finish playing
+        pass
+    
     return
 
 if __name__ == "__main__":
