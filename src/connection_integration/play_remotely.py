557cecb47d285a6e589b5c666c5fcefb3fd60249
Lilly Chiavetta <lhc24@duke.edu>
2024-12-06 16:57:15 -0500

    checkers works!

--- src/connection_integration/play_remotely.py
+++ src/connection_integration/play_remotely.py
@@ -31,12 +31,12 @@ def adapt_to_robot(message):
     if len(jumps) > 0:
         message_moves = message.split(",")
         for i in range(len(jumps)):
-            message_moves[i] = message_moves[i] + "J" + jumps[i]
+            message_moves[i] = message_moves[i] + " J" + jumps[i]
         message = ",".join(message_moves)
     
-    if "," in message:
-        print("Robot message: ", message)
-        exit()
+    # if "," in message:
+    #     print("Robot message: ", message)
+    #     exit()
 
     return message
 
@@ -52,32 +52,34 @@ def play_with_robot(game, socket):
             # message = game.ai_turn(difficulty="LLM")
             message = game.ai_turn(difficulty="Minimax", minimax_depth=3)
             # self.user_turn()
-            message = adapt_to_robot(message)
-            print(message)
 
             #if the robot is playing, wait for 5 seconds (comment this out if user is playing)
             user = False
-            if socket and not user: 
-                # Send the message to the server
-                client_socket.send(message.encode('utf-8'))
-                # Wait for the robot to finish its turn
-                sleep(5)
+            if not user:
+                message = adapt_to_robot(message)
+                print(message)
+                if socket: 
+                    # Send the message to the server
+                    client_socket.send(message.encode('utf-8'))
+                    # Wait for the robot to finish its turn
+                    sleep(25)
         else:
             # message = game.ai_turn(difficulty="Random")
-            # message = game.ai_turn(difficulty="Prefer Jumps")
-            message = game.ai_turn(difficulty="LLM")
+            message = game.ai_turn(difficulty="Prefer Jumps")
+            # message = game.ai_turn(difficulty="LLM")
             # message = game.ai_turn(difficulty="Minimax", minimax_depth=3)
             # game.user_turn()
-            message = adapt_to_robot(message)
-            print(message)
 
-            #if the robot is playing, wait for 5 seconds
+            #if the robot is playing, wait for 5 seconds (comment this out if user is playing)
             user = False
-            if socket and not user: 
-                # Send the message to the server
-                client_socket.send(message.encode('utf-8'))
-                # Wait for the robot to finish its turn
-                sleep(5)
+            if not user:
+                message = adapt_to_robot(message)
+                print(message)
+                if socket: 
+                    # Send the message to the server
+                    client_socket.send(message.encode('utf-8'))
+                    # Wait for the robot to finish its turn
+                    sleep(25)
 
         if game.check_winner():
             message = "exit"
@@ -89,15 +91,20 @@ def play_with_robot(game, socket):
 
         #see if user wants to exit
         # exit_message = input("Do you want to continue? (y/n): ")
-        exit_message = "y"
-        if exit_message == "n":
-            message = "exit"
-            if socket: client_socket.send(message.encode('utf-8'))
-            return message
+        # # exit_message = "y"
+        # if exit_message != "n":
+        #     if exit_message == "away":
+        #         message = "away"
+        #         if socket: client_socket.send(message.encode('utf-8'))
+        #     continue
+        # else:
+        #     message = "exit"
+        #     if socket: client_socket.send(message.encode('utf-8'))
+        #     return message
 
 if __name__ == "__main__":
-    # client_socket = rc.connect_to_robot()
-    client_socket = None #for testing only (if you don't have a robot to connect to)
+    client_socket = rc.connect_to_robot()
+    # client_socket = None #for testing only (if you don't have a robot to connect to)
     #start the game
     game = Game()
 
